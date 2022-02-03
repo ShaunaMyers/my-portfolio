f73ef0e6dd6e9c22f7db62c1f48a4aa0557dfcfb
Shauna Myers <shaunamyers26@gmail.com>
2022-02-03 15:19:08 -0800

    Initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Welcome to the portfolio of Shauna Myers</h1>
+        <a href='https://github.com/ShaunaMyers/my-portfolio'>GitHub</a>
+        <a href='https://www.linkedin.com/in/shauna-myers/'>LinkedIn</a>
+      </header>
+      <main>
+        <h2>Projects will go here</h2>
+      </main>
+    </>
+  );
+}
+
+export default App;
